b53b6ebe923f44162a1e105881dbcc47e1d339e1
Jason Racine <jjj.racine@gmail.com>
2013-04-19 08:03:58 +0000

    Cleaned and commented class Map

--- src/ch/hearc/p2/battleforatlantis/gameengine/Map.java
+++ src/ch/hearc/p2/battleforatlantis/gameengine/Map.java
@@ -13,35 +13,60 @@ import ch.hearc.p2.battleforatlantis.ui.PanelPrepare;
 
 public class Map extends JPanel
 {
+	/**
+	 * Number of columns
+	 */
 	private int width;
+	
+	/**
+	 * Number of lines
+	 */
 	private int height;
-
+	
+	/**
+	 * Map type (surface, submarine, atlantis)
+	 */
 	private MapType type;
-
-	Box[][] boxes;
-
+	
+	/**
+	 * Array of boxes which compose the map, in order [line, column]
+	 */
+	private Box[][] boxes;
+	
+	/**
+	 * Listener for click on boxes in preparation panel
+	 */
 	private MouseListener preparationListener;
+	
+	/**
+	 * Listener for click on boxes in game panel
+	 */
 	private MouseListener gameListener;
 	
+	/**
+	 * Debug logger
+	 */
 	private Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	/**
 	 * Create a map of specified dimensions
 	 * 
-	 * @param width
-	 *            Number of rows
-	 * @param height
-	 *            Number of columns
+	 * @param width Number of rows
+	 * @param height Number of columns
+	 * @param type Type of map (surface, submarine, atlantis)
 	 */
 	public Map(int width, int height, MapType type)
 	{
+		// Input fields
 		this.width = width;
 		this.height = height;
 		this.type = type;
 
+		// Prepare boxes lists (internal and display)
 		boxes = new Box[height][width];
 		setLayout(new GridLayout(height, width, 0, 0));
 
+		// Create and add boxes
 		for (int i = 0; i < height; i++)
 		{
 			for (int j = 0; j < width; j++)
@@ -51,6 +76,77 @@ public class Map extends JPanel
 			}
 		}
 
+		// Instanciate listeners
+		instanciatePreparationListener();
+		instanciateGameListener();
+	}
+
+	/**
+	 * Register boxes for preparation actions
+	 */
+	public void setPreparationListeners()
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				boxes[i][j].removeMouseListener(gameListener);
+				boxes[i][j].addMouseListener(preparationListener);
+			}
+		}
+	}
+
+	/**
+	 * Register boxes for game actions
+	 */
+	public void setGameListeners()
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				boxes[i][j].removeMouseListener(preparationListener);
+				boxes[i][j].addMouseListener(gameListener);
+			}
+		}
+	}
+	
+	/**
+	 * Get the box at specified coordinates (top-left is 0;0)
+	 * 
+	 * @param x Horizontal coordinate, starting at 0 on left side
+	 * @param y Vertical coordinate, starting at 0 on upper side
+	 * 
+	 * @return Box at location, or null if incorrect location
+	 */
+	public Box getBox(int x, int y)
+	{
+		if ((x>=0) && (x<this.width) && (y>=0) && (y<this.height))
+		{
+			return this.boxes[y][x];
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	/**
+	 * Get the current type of map
+	 * 
+	 * @return Map type
+	 */
+	public MapType getType()
+	{
+		return type;
+	}
+
+
+	/**
+	 * Instanciate boxes listener for preparation state
+	 */
+	private void instanciatePreparationListener()
+	{
 		preparationListener = new MouseAdapter()
 		{
 			@Override
@@ -85,7 +181,13 @@ public class Map extends JPanel
 				}
 			}
 		};
+	}
 
+	/**
+	 * Instanciate boxes listener for game state
+	 */
+	private void instanciateGameListener()
+	{
 		gameListener = new MouseAdapter()
 		{
 			@Override
@@ -106,45 +208,5 @@ public class Map extends JPanel
 
 			}
 		};
-	}
-
-	public void setPreparationListeners()
-	{
-		for (int i = 0; i < height; i++)
-		{
-			for (int j = 0; j < width; j++)
-			{
-				boxes[i][j].removeMouseListener(gameListener);
-				boxes[i][j].addMouseListener(preparationListener);
-			}
-		}
-	}
-
-	public void setGameListeners()
-	{
-		for (int i = 0; i < height; i++)
-		{
-			for (int j = 0; j < width; j++)
-			{
-				boxes[i][j].removeMouseListener(preparationListener);
-				boxes[i][j].addMouseListener(gameListener);
-			}
-		}
-	}
-	
-	public Box getBox(int x, int y)
-	{
-		if ((x>=0) && (x<this.width) && (y>=0) && (y<this.height))
-		{
-			return this.boxes[y][x];
-		}
-		else
-		{
-			return null;
-		}
-	}
-	public MapType getType()
-	{
-		return type;
 	}
 }
